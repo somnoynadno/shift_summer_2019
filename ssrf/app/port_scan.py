84ac340195ec278ecf211e8bcc9ea8457b258dbd
Alexander Krysov <krisov_sasha@mail.ru>
2019-07-02 17:43:41 +0700

    ssrf: some fixes

--- ssrf/app/port_scan.py
+++ ssrf/app/port_scan.py
@@ -1,6 +1,8 @@
 import requests
 
 for x in range(1,65536):
-	# r = requests.get('http://127.0.0.1/get_url_curl?url=127.0.0.1:'+str(x))
-	r = requests.get('http://127.0.0.1/get_url_requests?url=127.0.0.1:'+str(x))
-	if r.text != '':print("port", x, "closed");
+	r = requests.get('http://127.0.0.1/get_url_requests?url=http://127.0.0.1:'+str(x)+'/')
+	if r.status_code != 200:
+		print("port", x, "closed");
+	else:
+		print("port", x, "open");
