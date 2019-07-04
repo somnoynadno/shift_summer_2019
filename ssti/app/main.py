e467073fe1e4c9039c77fd480728050284b6ae8c
ODB686 <griboed228@gmail.com>
2019-07-03 22:52:18 -0400

    :)

--- ssti/app/main.py
+++ ssti/app/main.py
@@ -21,16 +21,6 @@ def unsafe_ssti():
 
 	return render_template_string(body, person=person)
 
-@app.route('/safe)
-def safe_ssti():
-	template = '''<title>No Injection Allowed!</title>
-		<a href={{ url_for('hello_xss')}}?name={{ name |e}}>
-		Click here for a welcome message</a>'''
-	name = "world"
-	if request.args.get('name'):
-		name = request.args.get('name')
-	return render_template_string(template, name=name)
-
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True, port=80)
